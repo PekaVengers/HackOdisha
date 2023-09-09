d1fbe262376ca8749da05a92bf94925124c07444
Kailash Choudhary <kailashjat604@gmail.com>
2023-09-09 10:08:31 +0530

    feat: add profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import Home from "./pages/Home"
 import Layout from "./components/Layout"
+import Profile from "./pages/Profile"
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
+    <Route path="/profile" element={<Profile />} />
   </Route>
 ))
 
