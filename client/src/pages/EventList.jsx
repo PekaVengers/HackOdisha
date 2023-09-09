2ae995b9402d32db7232789031af3bc2f009fe0e
Vidip Ghosh <vidip2001@gmail.com>
2023-09-09 21:42:42 +0530

    Events page improved

--- client/src/pages/EventList.jsx
+++ client/src/pages/EventList.jsx
@@ -12,19 +12,19 @@ export default function EventList() {
   console.log(data);
 
   return (
-    <div>
-      {
-        data?.map(event => (
+    <div className="flex flex-col items-center">
+      <div className="border-solid border-2 border-green-500 p-3 m-3 inline-block rounded-br-xl	rounded-tl-xl">
+        {data?.map(event => (
           <div key={event.id}>
-            
-            <h1 className="font-bold">{event.name}</h1>
-            <p>{event.organization}</p>
-            <p>{event.location}</p>
-            <p>{event.time}</p>
-            <p>{event.description}</p>
+            <h1 className="text-2xl font-bold mb-2">{event.name}</h1>
+            <p className="text-gray-600">{event.organization}</p>
+            <p className="text-gray-600">{event.location}</p>
+            <p className="text-gray-600">{event.time}</p>
+            <p className="text-gray-800 mt-2">{event.description}</p>
           </div>
-        ))
-      }
+        ))}
+      </div>
     </div>
+
   )
 }
