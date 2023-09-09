573988f6bd194b99e016c92e1398dbcf11b6b11e
Vidip Ghosh <vidip2001@gmail.com>
2023-09-09 12:15:32 +0530

    Button improved

--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
   return (
-    <button onClick={() => loginWithRedirect()} className="">
+    <button onClick={() => loginWithRedirect()} className="bg-green-400 p-2 text-white rounded-lg">
       Log In
     </button>
   )
--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function LogoutButton() {
   const {logout} = useAuth0();
   return (
-    <button onClick={() => logout()} className="">
+    <button onClick={() => logout()} className="bg-green-400 p-2 text-white rounded-lg font-bold">
       Log Out
     </button>
   )
