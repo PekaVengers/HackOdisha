fc5b9782eef3c4469d00583f80963c7740aa9607
Palak <Palakgoyal0304@gmail.com>
2023-09-09 19:30:36 +0530

    Add information accordion to save at home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,17 @@ import Profile from "./pages/Profile"
 import TreePlantation, {action as treePlantationAction} from "./pages/TreePlantation"
 import CreateEvent from "./pages/CreateEvent"
 import PlantedTrees, {loader as plantedTreesLoader} from "./pages/PlantedTrees"
+import SaveAtHome from "./pages/SaveAtHome"
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="profile" element={<Profile />} />
+    <Route path="save-at-home" element={<SaveAtHome />} />
     <Route action={treePlantationAction} path="tree-plantation" element={<TreePlantation />} />
     <Route loader={plantedTreesLoader} path="planted-trees" element={<PlantedTrees />} />
     <Route action={CreateEvent} path="create-event" element={<CreateEvent />} />
+    <Route path="*" element={<p>404 Not Found</p>} />
   </Route>
 ))
 
---
+++ client/src/pages/SaveAtHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SaveAccordion from '../components/saveAtHome/saveAccordion'
+
+const SaveAtHome = () => {
+  return (
+    <div>
+      <SaveAccordion />
+    </div>
+  )
+}
+
+export default SaveAtHome
