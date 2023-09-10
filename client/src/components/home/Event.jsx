58ae2624c29f1378a782bd02645f72374997f37d
LostZoro476 <zoro80316@gmail.com>
2023-09-10 12:54:02 +0530

    Fixed minor bugs

--- client/src/components/home/Event.jsx
+++ client/src/components/home/Event.jsx
@@ -9,9 +9,9 @@ const Event = () => {
       <section className='text-gray-600 body-font py-1'>
         <div className='container mx-auto flex px-8 md:flex-row flex-col items-center'>
           <div className='px-10 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
-            <h1 className='text-4xl md:text-5xl mb-4 font-bold text-gray-900'>RRR - Reduce, Reuse, Recycle</h1>
+            <h1 className='text-4xl md:text-5xl mb-4 font-bold text-gray-900'>Events</h1>
             <p className='mb-8 leading-relaxed text-xl font-normal md:w-3/4 text-center md:text-left'>
-            Easily craft and promote your eco-friendly events. Connect with fellow environmental enthusiasts. Join a community dedicated to making a positive impact on our planet, and together, let's create a greener and more sustainable future.
+            Easily craft and promote your eco-friendly events, connecting with fellow environmental enthusiasts. Discover innovative ways to make a positive impact on the planet through shared experiences, and join a community dedicated to creating a sustainable and greener future for all.
             </p>
             <div className='flex justify-center'>
               <Link
