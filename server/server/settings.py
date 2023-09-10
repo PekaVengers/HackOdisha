95c87b68becd330c2c1cc6281ef84716b7019a6f
Kailash Choudhary <kailashjat604@gmail.com>
2023-09-10 12:49:57 +0530

    change production database

--- server/server/settings.py
+++ server/server/settings.py
@@ -89,19 +89,19 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
+}
+
+if not DEBUG:
     DATABASES['default'] = dj_database_url.config(
     default=os.getenv("DATABASE_URL"),
     conn_max_age=600,
     conn_health_checks=True,
-)
+    )
 
 
 # Password validation
